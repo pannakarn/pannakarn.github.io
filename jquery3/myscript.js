59bccc70490a45e5b33ed239e9b0ebf8e2959eef
pannakarn <pannakarn.p@ku.th>
2019-02-09 21:43:12 +0700

    jquery3

--- jquery3/myscript.js
+++ jquery3/myscript.js
@@ -1,90 +1,150 @@
 $(document).ready(function() {
     $('#submit-btn').click(function() {
-        // console.log('click on submit button');
-        // console.log($('#input-text').val());
-        // console.log($('#input-text').val().includes("may"));
-
         var keyword = $('#input-text').val();
 
         $("#data_table td").remove();
 
-        console.log(keyword);
         $.ajax({
             url: "data.json",
             dataType: "json"
         }).done(function(response) {
-            console.log('ok');
+
             response.forEach(element => {
 
                 var keyUpper = keyword.toUpperCase();
 
-                if(element.fansiteName.toUpperCase().includes(keyUpper) || element.type.toUpperCase().includes(keyUpper) || 
+                if(element.name.toUpperCase().includes(keyUpper) ||
+                    element.fansiteName.toUpperCase().includes(keyUpper) || element.type.toUpperCase().includes(keyUpper) || 
                     element.goodsName.toUpperCase().includes(keyUpper) || element.code.toUpperCase().includes(keyUpper) || 
-                    element.amount == keyword || element.period.toUpperCase().includes(keyUpper) ||
                     element.derivery.toUpperCase().includes(keyUpper) || element.status.toUpperCase().includes(keyUpper)) {
 
                     var row = $("<tr />")
                     $("#data_table").append(row);
+                    row.append($("<td>" + element.name + "</td>"));
                     row.append($("<td>" + element.fansiteName + "</td>"));
                     row.append($("<td>" + element.type + "</td>"));
                     row.append($("<td>" + element.goodsName + "</td>"));
                     row.append($("<td>" + element.code + "</td>"));
-                    row.append($("<td id=\"amount\">" + element.amount + "</td>"));
-                    row.append($("<td>" + element.period + "</td>"));
                     row.append($("<td>" + element.derivery + "</td>"));
-                    row.append($("<td>" + element.status + "</td>"));
-
-                    console.log('find');
-                    console.log(element.fansiteName);
+                    row.append($("<td id=\"status-column\">" + element.status + "</td>"));
                 }
 
-
-            
             });
         });
 
     });
 
-
-
-    // $('#submit-btn').keypress(function (e) {
-    //     var key = e.which;
-    //     if(key == 13)  // the enter key code
-    //     {
-    //     $('input[name = butAssignProd]').click();
-    //         return false;  
-    //     }
-    // });
-
     $('#input-text').keypress(function(event){
-    var keycode = (event.keyCode ? event.keyCode : event.which);
-    if(keycode == '13'){
-        $('#submit-btn').click();
-    }
-});   
-
-
+        var keycode = (event.keyCode ? event.keyCode : event.which);
+        if(keycode == '13'){
+            $('#submit-btn').click();
+        }
+    });   
 
     $.ajax({
         url: "data.json",
         dataType: "json"
     }).done(function(response) {
-        // console.log(response);
+        console.log(response);
         response.forEach(element => {
-            // console.log(element.fansiteName, element.type, element.goodsName, element.code, element.amount, element.period, 
-            //     element.derivery, element.status);
             var row = $("<tr />")
             $("#data_table").append(row);
+            row.append($("<td>" + element.name + "</td>"));
             row.append($("<td>" + element.fansiteName + "</td>"));
             row.append($("<td>" + element.type + "</td>"));
             row.append($("<td>" + element.goodsName + "</td>"));
             row.append($("<td>" + element.code + "</td>"));
-            row.append($("<td id=\"amount\">" + element.amount + "</td>"));
-            row.append($("<td>" + element.period + "</td>"));
             row.append($("<td>" + element.derivery + "</td>"));
-            row.append($("<td>" + element.status + "</td>"));
+            row.append($("<td id=\"status-column\">" + element.status + "</td>"));
         });
     });
 
-});
 
+    $('#advance-btn').click(function() {
+        $('.advance-inner').fadeIn();
+    });
+
+    $('#advance-submit-btn').click(function() {
+        var name = $('#name-text').val().toUpperCase();
+        var fansiteName = $('#fansite-text').val().toUpperCase();
+        var type = $('#type-text').val().toUpperCase();
+        var derivery = $('#derivery-text').val().toUpperCase();
+        var status = $('#status-text').val().toUpperCase();
+        var goodsName = $('#goods-name-text').val().toUpperCase();
+        var code = $('#code-text').val().toUpperCase();
+
+        $("#data_table td").remove();
+
+
+        $.ajax({
+            url: "data.json",
+            dataType: "json"
+        }).done(function(response) {
+
+            response.forEach(element => {
+
+                if( element.name.toUpperCase().includes(name) && element.goodsName.toUpperCase().includes(goodsName) &&
+                    element.code.toUpperCase().includes(code) &&
+                    element.fansiteName.toUpperCase().includes(fansiteName) && element.type.toUpperCase().includes(type) && 
+                    element.derivery.toUpperCase().includes(derivery) && element.status.toUpperCase().includes(status)) {
+    
+                            var row = $("<tr />")
+                            $("#data_table").append(row);
+                            row.append($("<td>" + element.name + "</td>"));
+                            row.append($("<td>" + element.fansiteName + "</td>"));
+                            row.append($("<td>" + element.type + "</td>"));
+                            row.append($("<td>" + element.goodsName + "</td>"));
+                            row.append($("<td>" + element.code + "</td>"));
+                            row.append($("<td>" + element.derivery + "</td>"));
+                            row.append($("<td id=\"status-column\">" + element.status + "</td>"));
+                    }
+            });
+        });
+    });
+
+
+    $('#name-text').keypress(function(event){
+        var keycode = (event.keyCode ? event.keyCode : event.which);
+        if(keycode == '13'){
+            $('#advance-submit-btn').click();
+        }
+    }); 
+    $('#fansite-text').keypress(function(event){
+        var keycode = (event.keyCode ? event.keyCode : event.which);
+        if(keycode == '13'){
+            $('#advance-submit-btn').click();
+        }
+    }); 
+    $('#type-text').keypress(function(event){
+        var keycode = (event.keyCode ? event.keyCode : event.which);
+        if(keycode == '13'){
+            $('#advance-submit-btn').click();
+        }
+    }); 
+    $('#derivery-text').keypress(function(event){
+        var keycode = (event.keyCode ? event.keyCode : event.which);
+        if(keycode == '13'){
+            $('#advance-submit-btn').click();
+        }
+    }); 
+    $('#status-text').keypress(function(event){
+        var keycode = (event.keyCode ? event.keyCode : event.which);
+        if(keycode == '13'){
+            $('#advance-submit-btn').click();
+        }
+    });
+    $('#code-text').keypress(function(event){
+        var keycode = (event.keyCode ? event.keyCode : event.which);
+        if(keycode == '13'){
+            $('#advance-submit-btn').click();
+        }
+    });
+    $('#goods-name-text').keypress(function(event){
+        var keycode = (event.keyCode ? event.keyCode : event.which);
+        if(keycode == '13'){
+            $('#advance-submit-btn').click();
+        }
+    });   
+
+
+});
